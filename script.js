e3b2261769980956e1e32c825e9f94f886690af8
Echezona <echezona.oji@gmail.com>
2021-09-29 09:38:38 +0100

    refactor openModal into a named function

--- script.js
+++ script.js
@@ -6,10 +6,7 @@ const overlay = document.querySelector(".overlay");
 const showModalBtns = document.querySelectorAll(".show-modal");
 
 showModalBtns.forEach((showModalBtn) => {
-  showModalBtn.addEventListener("click", () => {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-  });
+  showModalBtn.addEventListener("click", openModal);
 });
 
 closeModalBtn.addEventListener("click", closeModal);
@@ -18,4 +15,9 @@ overlay.addEventListener("click", closeModal);
 function closeModal() {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
+}
+
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
 }
